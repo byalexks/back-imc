4da874646004215c4bbf6e97bbee7e0a8ccd9eb0
Ronal2 <ronal2.gutierrez@gmail.com>
2020-08-04 23:28:18 -0500

    Conectando base de datos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express()
-const PORT = 8282
+const app = express();
 
-app.use(require('./routes/loginRoutes'))
+const PORT = 8282;
+const URL_DB = "mongodb://localhost:27017/user";
 
+app.use(require("./routes/loginRoutes"));
 
-app.listen(PORT, ( ) => {
-    console.log(`Server on port ${PORT}`)
-})
+mongoose.connect(
+  URL_DB,
+  { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true },
+  (err, res) => {
+    if (err) {
+      return console.log(err);
+    }
+    return console.log("connection DB");
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`);
+});
